b3fe4f36057588ec927c1fc1f36747f9a47bed3f
NicolasQueiroga <nicolasqueiroga@me.com>
2025-05-09 02:32:36 -0300

    Add logger setup functionality with colorful console output

---
+++ source/configs/logger.py
@@ -0,0 +1,36 @@
+import logging
+from colorlog import ColoredFormatter
+
+def setup_logger(name: str, log_file: str = "app.log", level: int = logging.INFO, topic: str = "") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    file_handler = logging.FileHandler(log_file)
+    console_handler = logging.StreamHandler()
+
+    # Define a colorful formatter for console output
+    color_formatter = ColoredFormatter(
+        f"%(log_color)s%(asctime)s - {topic} - %(name)s - %(levelname)s - %(message)s",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
+    )
+
+    # Define a plain formatter for file output
+    plain_formatter = logging.Formatter(
+        f"%(asctime)s - {topic} - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    file_handler.setFormatter(plain_formatter)
+    console_handler.setFormatter(color_formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+main_logger = setup_logger("semantic-segmentation-trainer", "trainer.log", topic="MAIN")
